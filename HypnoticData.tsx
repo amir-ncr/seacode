5cc2c81e4db379735251647cc62c167c4cd2bcdf
Mohamed Rafik <rafikbn666@gmail.com>
2024-11-04 20:43:37 +0100

    Update HypnoticData.tsx

--- HypnoticData.tsx
+++ HypnoticData.tsx
@@ -5,8 +5,8 @@ import {
   Info,
   UserRoundPen,
 } from "lucide-react";
-import {LinkType, QATYPE, SectionTwoType} from "./HypnoticTypes";
-import {TfiWorld} from "react-icons/tfi";
+import { LinkType, QATYPE, SectionTwoType } from "./HypnoticTypes";
+import { TfiWorld } from "react-icons/tfi";
 
 const IconSize = 8;
 
@@ -89,60 +89,7 @@ export const SectionTwoData: SectionTwoType = [
 
 export const QADATA: QATYPE = [
   {
-    question: "What is uvibe?",
-    answer:
-      "uvibe is an AI-powered personal stylist that helps you curate stylish outfits based on your preferences, body type, and occasion.",
-  },
-  {
-    question: "How does uvibe work?",
-    answer:
-      "Simply provide information about your style preferences, body type, and occasion, and uvibe will suggest personalized outfit combinations.",
-  },
-  {
-    question: "Is uvibe available on both web and mobile?",
-    answer:
-      "Yes, uvibe is available as both a web application and a mobile app, making it convenient to access anytime, anywhere.",
-  },
-  {
-    question:
-      "Can uvibe suggest outfits for specific occasions like weddings, parties, or casual outings?",
-    answer:
-      "Absolutely! uvibe can tailor outfit suggestions to various events, ensuring you always look your best.",
-  },
-  {
-    question: "Can I save my favorite outfits or styles?",
-    answer:
-      "Yes, you can create a personal style board to save outfits you like and refer to them later.",
-  },
-  {
-    question: "Does uvibe consider my budget when suggesting outfits?",
-    answer:
-      "You can set a budget preference within the app, and uvibe will provide outfit options that align with your financial constraints.",
-  },
-  {
-    question:
-      "Can I request specific items or brands to be included in my outfit suggestions?",
-    answer:
-      "Yes, you can input specific items or brands you prefer, and uvibe will incorporate them into your outfit recommendations.",
-  },
-  {
-    question: "Is uvibe compatible with all devices and operating systems?",
-    answer:
-      "uvibe is designed to be compatible with most modern devices and operating systems.",
-  },
-  {
-    question: "Does uvibe require an internet connection to function?",
-    answer:
-      "Yes, an internet connection is necessary to access uvibe's features and receive outfit suggestions.",
-  },
-  {
-    question: "How often does uvibe update its outfit recommendations?",
-    answer:
-      "uvibe's algorithm is constantly evolving, so you can expect new and updated outfit suggestions over time.",
-  },
-  {
-    question: "Can I provide feedback on uvibe's recommendations?",
-    answer:
-      "Yes, you can provide feedback on outfit suggestions to help improve uvibe's accuracy and personalization.",
+    question: "Nigga How?",
+    answer: "lame ass niggas on the gruind",
   },
 ];
